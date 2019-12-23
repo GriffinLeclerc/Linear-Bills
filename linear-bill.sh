ee5c1ba0e68a27b7f2df3b682ae47a365dea812f
Griffin Leclerc <leclercgriffin@gmail.com>
2019-12-23 12:52:04 -0500

    add gitignore, clean up linear bill script

--- linear-bill.sh
+++ linear-bill.sh
@@ -1,1 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./linear-bill.sh <bills>"
+    echo "where <bills> is the number of times the sequence should be repeated."
+    exit 0
+fi
+
 python3 src/repeatBill.py $1
